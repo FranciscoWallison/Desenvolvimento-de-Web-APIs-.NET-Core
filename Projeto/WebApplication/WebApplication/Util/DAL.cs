498ca90baa927155e47002614f4d49f3cc89e096
Wallison <franciscowallison@gmail.com>
2019-05-18 20:14:12 -0300

    create config db

---
+++ Projeto/WebApplication/WebApplication/Util/DAL.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication.Util
+{
+    public class DAL
+    {
+        private string Server = "localhost";
+        private string Database = "";
+        private string User = "root";
+        private string Password = "";
+    }
+}
