4efd24a986c57fc9ad08bb2eb6a1f716cf8ee84d
Wallison <franciscowallison@gmail.com>
2019-05-19 12:06:31 -0300

    Web API - Método Registrar Clientes
    Criando estrutura de acesso e manipulação de dados.

--- Projeto/WebApplication/WebApplication/Util/DAL.cs
+++ Projeto/WebApplication/WebApplication/Util/DAL.cs
@@ -2,14 +2,43 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Data;
 
 namespace WebApplication.Util
 {
     public class DAL
     {
-        private string Server = "localhost";
-        private string Database = "";
-        private string User = "root";
-        private string Password = "";
+        private static string Server = "localhost";
+        private static string Database = "DBCLIENTE";
+        private static string User = "root";
+        private static string Password = "";
+        private MySqlConnection Connection;
+
+        private string ConnectionString = $"Server={Server};Database={Database};Uid={User};Pwd={Password};Sslmode=none;";
+
+        public DAL()
+        {
+            Connection = new MySqlConnection(ConnectionString);
+            Connection.Open();
+        }
+
+        //Executa: INSERT, UPTADE, DELETE
+        public void ExecutarComandoSQL(string sql)
+        {
+            MySqlCommand Command = new MySqlCommand(sql, Connection);
+            Command.ExecuteNonQuery();
+        }
+
+        //Retorna Dados do Banco
+        public DataTable RetornarDataTable(string sql)
+        {
+            MySqlCommand Command = new MySqlCommand(sql, Connection);
+            MySqlDataAdapter DataAdapter = new MySqlDataAdapter(Command);
+            DataTable Dados = new DataTable();
+            DataAdapter.Fill(Dados);
+            return Dados;
+        }
     }
+
 }
