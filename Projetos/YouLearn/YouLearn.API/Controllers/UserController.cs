b7dcd183cc8924d2b3f2e411f31fc1c2e724d7f7
Wallison <franciscowallison@gmail.com>
2019-07-14 19:08:50 -0300

    Inserindo um usuário no banco de dados

---
+++ Projetos/YouLearn/YouLearn.API/Controllers/UserController.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Security.Principal;
+using System.Threading.Tasks;
+using YouLearn.Api.Security;
+using YouLearn.Domain.Arguments.User;
+using YouLearn.Domain.Interfaces.Services;
+using YouLearn.Infra.Transactions;
+
+namespace YouLearn.Api.Controllers
+{
+    public class UsuarioController : Base.ControllerBase
+    {
+        private readonly IServiceUser _serviceUser;
+
+        public UsuarioController(IUnitOfWork unitOfWork, IServiceUser serviceUser) : base(unitOfWork)
+        {
+            _serviceUser = serviceUser;
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        [Route("api/v1/Usuario/Adicionar")]
+        public async Task<IActionResult> Adicionar([FromBody]AuthenticateUserRequest request)
+        {
+            try
+            {
+                var response = _serviceUser.AuthenticateUser(request);
+                return await ResponseAsync(response, _serviceUser);
+            }
+            catch (Exception ex)
+            {
+                return await ResponseExceptionAsync(ex);
+            }
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        [Route("api/v1/Usuario/Autenticar")]
+        public object Autenticar(
+            [FromBody]AuthenticateUserRequest request,
+            [FromServices]SigningConfigurations signingConfigurations,
+            [FromServices]TokenConfigurations tokenConfigurations)
+        {
+            bool credenciaisValidas = false;
+            AuthenticateUserResponse response = _serviceUser.AuthenticateUser(request);
+
+            credenciaisValidas = response != null;
+
+            if (credenciaisValidas)
+            {
+                ClaimsIdentity identity = new ClaimsIdentity(
+                    new GenericIdentity(response.Id.ToString(), "Id"),
+                    new[] {
+                        new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString("N")),
+                        //new Claim(JwtRegisteredClaimNames.UniqueName, response.Usuario)
+                        new Claim("Usuario", JsonConvert.SerializeObject(response))
+                    }
+                );
+
+                DateTime dataCriacao = DateTime.Now;
+                DateTime dataExpiracao = dataCriacao +
+                    TimeSpan.FromSeconds(tokenConfigurations.Seconds);
+
+                var handler = new JwtSecurityTokenHandler();
+                var securityToken = handler.CreateToken(new SecurityTokenDescriptor
+                {
+                    Issuer = tokenConfigurations.Issuer,
+                    Audience = tokenConfigurations.Audience,
+                    SigningCredentials = signingConfigurations.SigningCredentials,
+                    Subject = identity,
+                    NotBefore = dataCriacao,
+                    Expires = dataExpiracao
+                });
+                var token = handler.WriteToken(securityToken);
+
+                return new
+                {
+                    authenticated = true,
+                    created = dataCriacao.ToString("yyyy-MM-dd HH:mm:ss"),
+                    expiration = dataExpiracao.ToString("yyyy-MM-dd HH:mm:ss"),
+                    accessToken = token,
+                    message = "OK",
+                    FirstName = response.FirstName
+                };
+            }
+            else
+            {
+                return new
+                {
+                    authenticated = false,
+                    _serviceUser.Notifications
+                };
+            }
+        }
+    }
+}
